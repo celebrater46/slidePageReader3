4be5aceb428e8a0859f70d68031b698b94867238
celebrater <tkgm45nbchk@gmail.com>
2022-03-07 13:48:50 +0900

    async testCreateBook() succeeded

--- books/getBook.js
+++ books/getBook.js
@@ -7,7 +7,7 @@ const convertTextToArray =(txt) => {
     });
 }
 
-const getBook = async(txt) => {
+const getBook = async(id, txt) => {
     // const href = location.href; // https://enin-world.sakura.ne.jp/index.html
     // const pathname = location.pathname; // /index.html
     // const textPath = href.replace(pathname, "") + "/books/bookList.txt";
@@ -15,8 +15,19 @@ const getBook = async(txt) => {
     const response = await fetch(textPath);
     const text = await response.text();
     const array = convertTextToArray(text);
+    const titleAndDir = array[id].split("|");
+    const book = new Book(id, titleAndDir[0], titleAndDir[1]);
     console.log("array: ");
     console.log(array);
 }
 
-getBook("bookList");
+const testCreateBook = async() => {
+    let book = new Book(1, "第三世界収容所", "prison");
+    await book.getChapters();
+    await book.getSubTitles();
+    console.log("book: ");
+    console.log(book);
+}
+
+// getBook(1, "bookList");
+testCreateBook();
---
+++ init.js
@@ -0,0 +1,2 @@
+const dir = "file:///J:/Dropbox/PC5_cloud/pg/js/vanilla/slidePageReader/";
+// const dir = "https://enin-world.sakura.ne.jp/files/app/demo/slidePageReader/";

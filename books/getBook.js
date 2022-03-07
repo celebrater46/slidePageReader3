72b2f55717f9e430a7841a684997250a028bc818
celebrater <tkgm45nbchk@gmail.com>
2022-03-07 21:39:40 +0900

    cleaned up code

--- books/getBook.js
+++ books/getBook.js
@@ -7,19 +7,19 @@ const convertTextToArray =(txt) => {
     });
 }
 
-const getBook = async(id, txt) => {
-    // const href = location.href; // https://enin-world.sakura.ne.jp/index.html
-    // const pathname = location.pathname; // /index.html
-    // const textPath = href.replace(pathname, "") + "/books/bookList.txt";
-    const textPath = "./books/" + txt + ".txt";
-    const response = await fetch(textPath);
-    const text = await response.text();
-    const array = convertTextToArray(text);
-    const titleAndDir = array[id].split("|");
-    const book = new Book(id, titleAndDir[0], titleAndDir[1]);
-    console.log("array: ");
-    console.log(array);
-}
+// const getBook = async(id, txt) => {
+//     // const href = location.href; // https://enin-world.sakura.ne.jp/index.html
+//     // const pathname = location.pathname; // /index.html
+//     // const textPath = href.replace(pathname, "") + "/books/bookList.txt";
+//     const textPath = "./books/" + txt + ".txt";
+//     const response = await fetch(textPath);
+//     const text = await response.text();
+//     const array = convertTextToArray(text);
+//     const titleAndDir = array[id].split("|");
+//     const book = new Book(id, titleAndDir[0], titleAndDir[1]);
+//     console.log("array: ");
+//     console.log(array);
+// }
 
 const testCreateBook = async() => {
     const textPath = "./books/texts/shiroganeki.txt";
@@ -28,13 +28,7 @@ const testCreateBook = async() => {
     const book = new Book(1);
     await book.init(text);
     await book.getArticles();
-    // localStorage["shiroganeki"] = book;
     localStorage.setItem("shiroganeki", JSON.stringify(book));
-    // await book.getChapters();
-    // await book.getSubTitles();
-    // console.log("book: ");
-    // console.log(book);
-    // console.log(localStorage.getItem("shiroganeki")); // [object object]
     const json = JSON.parse(localStorage.getItem("shiroganeki"));
     console.log(json); // succeeded
 }

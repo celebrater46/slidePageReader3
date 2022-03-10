6d9fa186ef252f95bbd2831a2db3308f3998c49d
celebrater <tkgm45nbchk@gmail.com>
2022-03-10 23:06:14 +0900

    togglePanel() succeeded

--- modules/slider.js
+++ modules/slider.js
@@ -1,3 +1,4 @@
+const div = document.getElementById("sliderDiv");
 const slider = document.querySelector('input[name="pageSlider"]');
 const pageNumP = document.getElementById("currentPageNum");
 
@@ -5,6 +6,22 @@ const changeSlider = () => {
     console.log(slider.value);
     pageNumP.innerText = slider.value;
     scroll(slider.value, true);
+}
+
+const togglePanel = () => {
+    console.log("did togglePanel()");
+    div.classList.toggle("in");
+    console.log("classList toggled");
+    console.log(slider.classList);
+    // if(slider.classList.contains('in')){
+    //     slider.classList.remove("in");
+    //     console.log("class removed");
+    //     console.log(slider.classList);
+    // } else {
+    //     slider.classList.add("in");
+    //     console.log("class added");
+    //     console.log(slider.classList);
+    // }
 }
 
 const setSlidersMax = (num) => {
